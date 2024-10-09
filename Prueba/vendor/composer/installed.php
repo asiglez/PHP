4bf52ae78176cafab29cca5d204771f222e95701
asiglez <asier.gonzalezgutierrez@iesmiguelherrero.com>
2024-10-09 18:15:28 +0200

    PHP

---
+++ Prueba/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'usuario/prueba',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e62f1e238a2214693f6ad3722cb997794e3fc9b2',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'hackzilla/password-generator' => array(
+            'pretty_version' => '1.7.0',
+            'version' => '1.7.0.0',
+            'reference' => '743c8493d2c33ebf9496a1c3c248efeb568fe134',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../hackzilla/password-generator',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'usuario/prueba' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e62f1e238a2214693f6ad3722cb997794e3fc9b2',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
